8c73e87a0e03b24b8dd287c32db870281a15ed8f
darkside <34475779+darkside-e@users.noreply.github.com>
2020-08-20 18:26:07 +0300

    update

--- Main_Data/phrases.py
+++ Main_Data/phrases.py
@@ -4,19 +4,18 @@ import json
 
 
 def get_grams(tp, n_range, limit):
-    ngrams = []
+    ngrams = {}
     for n in n_range:
         series = tp.most_frequent_n_grams(n=n, limit=limit)
-        res = {}
         for index, value in series.items():
-            res[str(index)] = int(value)
-        ngrams.append(res)
+            ngrams[str(index)] = int(value)
     return ngrams
 
 
 def pre_processing(df, stop_words):
     tp = TextProcessing(df, 'text')  # translated_text
     tp.pre_process.remove_non_alphabet()
+    tp.pre_process.to_lower_case()
     if stop_words:
         tp.pre_process.remove_stop_words(stop_words)
     return tp
@@ -40,9 +39,9 @@ def kaddaas_phrases(df, stop_words):
     for kaddaa in kadaas:
         temp_df = (df[df['KADAA_ID'] == kaddaa]).copy()
         tp = pre_processing(temp_df, stop_words)
-        kaddaa_dict[int(kaddaa)] = get_grams(tp, [2, 3, 4, 5], 50)
+        kaddaa_dict[int(kaddaa)] = get_grams(tp, [2, 3], 25)
 
-    with open('kaddaas_phrases.json', 'w', encoding='utf-8') as f:
+    with open('kaddaas_phrases3.json', 'w', encoding='utf-8') as f:
         f.write(json.dumps(kaddaa_dict, indent=2, ensure_ascii=False))
 
 
@@ -52,10 +51,29 @@ def mo7afazat_phrases(df, stop_words):
     for mohafaza in mohafazat:
         temp_df = (df[df['MOHAFAZA_ID'] == mohafaza]).copy()
         tp = pre_processing(temp_df, stop_words)
-        mohafaza_dict[int(mohafaza)] = get_grams(tp, [2, 3, 4, 5], 50)
+        mohafaza_dict[int(mohafaza)] = get_grams(tp, [2, 3], 25)
 
-    with open('mo7afazat_phrases.json', 'w', encoding='utf-8') as f:
+    with open('mo7afazat_phrases3.json', 'w', encoding='utf-8') as f:
         f.write(json.dumps(mohafaza_dict, indent=2, ensure_ascii=False))
+
+
+def get_stop_words(phrases_json):
+    with open(phrases_json, 'r', encoding='utf-8') as f:
+        data = json.loads(f.read())
+
+    freq = {}
+    for k, v in data.items():
+        for k1, v1 in v.items():
+            text = k1.split()
+            for t in text:
+                if t in freq:
+                    freq[t] += 1
+                else:
+                    freq[t] = 1
+
+    freq = sorted(freq.items(), key=lambda x: x[1], reverse=True)
+    with open('stop_words.json', 'w', encoding='utf-8') as f:
+        f.write(json.dumps(freq, indent=2, ensure_ascii=False))
 
 
 def mo7afazat(df):
@@ -83,9 +101,23 @@ def kaddaas(df):
 
 
 if __name__ == '__main__':
-    df = pd.read_csv('all.csv')
-    mo7afazat_phrases(df, None)
-    kaddaas_phrases(df, None)
+    # df = pd.read_csv('all.csv')
+    #
+    # searches = [
+    #     'corona',
+    #     'covid-19',
+    #     'كورونا',
+    #     'كورونا_لبنان',
+    #     'healthcare',
+    #     'medical'
+    # ]
+    #
+    # stop_words = searches + ['http']
+    #
+    # mo7afazat_phrases(df, stop_words)
+    # kaddaas_phrases(df, stop_words)
 
     # mo7afazat(df)
     # kaddaas(df)
+
+    get_stop_words('kaddaas_phrases2.json')
